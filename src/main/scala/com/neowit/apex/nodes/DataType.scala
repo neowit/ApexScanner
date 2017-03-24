9ce7da1b83216ff0965823727f2d5788e62fd863
A. G <catch+github@neowit.com>
2017-03-24 04:26:20 +0000

    simplified grammar related to “implements” and complex/compound types

--- src/main/scala/com/neowit/apex/nodes/DataType.scala
+++ src/main/scala/com/neowit/apex/nodes/DataType.scala
@@ -1,8 +1,27 @@
 package com.neowit.apex.nodes
 
-case class DataType(qualifiedName: QualifiedNameNode, typeArgumentsOpt: Option[TypeArgumentsNode], locationInterval: LocationInterval) extends AstNode {
+trait DataTypeBase extends AstNode {
+
     override def nodeType: AstNodeType = DataTypeNodeType
-    def text: String = qualifiedName.text + typeArgumentsOpt.map(_.text).getOrElse("")
+    def text: String
+    override def getDebugInfo: String = super.getDebugInfo + " " + text
+}
+case class DataType(qualifiedName: QualifiedNameNode, typeArgumentsOpt: Option[TypeArgumentsNode],
+                    locationInterval: LocationInterval) extends DataTypeBase {
+    def text: String =
+        qualifiedName.text +
+            typeArgumentsOpt.map(_.text).getOrElse("")
+
+}
+
+//VOID
+case class DataTypeVoid(locationInterval: LocationInterval) extends DataTypeBase {
+    override def text: String = "void"
+}
+
+//Some[] - array type
+case class DataTypeArray(qualifiedName: QualifiedNameNode, locationInterval: LocationInterval) extends DataTypeBase {
+    def text: String = qualifiedName.text + "[]"
 }
 
 
