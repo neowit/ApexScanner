67b885078241036500d1c3fdfb58abaa7c0c4e61
A. G <catch+github@neowit.com>
2017-03-24 05:25:23 +0000

    should have been committed earlier
    as part of renaming EmptyNode to NullNode

--- src/main/scala/com/neowit/apex/nodes/NullNode.scala
+++ src/main/scala/com/neowit/apex/nodes/NullNode.scala
@@ -3,5 +3,6 @@ package com.neowit.apex.nodes
 object NullNode extends AstNode {
     override def locationInterval: LocationInterval = LocationInterval.INVALID_LOCATION
 
-    override def nodeType: AstNodeType = EmptyNodeType
+    override def nodeType: AstNodeType = NullNodeType
+    override def getDebugInfo: String = super.getDebugInfo
 }
