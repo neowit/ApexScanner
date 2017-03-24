00984d150c3c66d100fdbaafb30cc62162db15e6
A. G <catch+github@neowit.com>
2017-03-24 03:28:16 +0000

    FinalNode is not necessary
    we do not need to keep details like “<” or “,”

--- src/main/scala/com/neowit/apex/nodes/QualifiedNameNode.scala
+++ src/main/scala/com/neowit/apex/nodes/QualifiedNameNode.scala
@@ -2,6 +2,6 @@ package com.neowit.apex.nodes
 
 case class QualifiedNameNode(locationInterval: LocationInterval) extends AstNode {
     override def nodeType: AstNodeType = QualifiedNameNodeType
-    def text: String = getChildren[FinalNode](FinalNodeType).map(_.text).mkString(".")
+    def text: String = getChildren[IdentifierNode](IdentifierNodeType).map(_.name).mkString(".")
     override def getDebugInfo: String = super.getDebugInfo + " " + text
 }
