neowit/ApexScanner

commit 5934cbcf7cb2a8c9846351002f91c4a6fba68e4a

update to scala 2.13.4 and fixes for warnings raised by compiler v2.13.4

src/main/scala/com/neowit/apexscanner/resolvers/AscendingDefinitionFinder.scala
@@ -124,13 +124,10 @@ class AscendingDefinitionFinder(actionContext: ActionContext) extends LazyLoggin
 
         // convert Seq[Future[Option[AstNode]]] to Future[Seq[Option[AstNode]]]
         val allDefNodes = nodeOpts.filter(_.isDefined).map(_.get)
-        allDefNodes.filter {
-            case defNode: AstNode with IsTypeDefinition => true
+        allDefNodes.map {
+            case _: AstNode with IsTypeDefinition => true
             case _ => false
-        }.map {
-            case defNode: AstNode with IsTypeDefinition =>
-                defNode
-        }
+        }.map(_.asInstanceOf[AstNode with IsTypeDefinition])
     }
 
     def findDefinition(target: AstNode, startNode: AstNode): Seq[AstNode] = {

build.sbt
@@ -4,7 +4,7 @@ name := "ApexScanner"
 version := "1.0"
 
 //scalaVersion := "2.12.10"
-scalaVersion := "2.13.1"
+scalaVersion := "2.13.4"
 
 //https://blog.threatstack.com/useful-scalac-options-for-better-scala-development-part-1
 scalacOptions ++= Seq(
@@ -22,6 +22,8 @@ scalacOptions ++= Seq(
     //"-Ywarn-unused-import",
     "-Ywarn-dead-code",
     //"-Ypartial-unification"
+    // Excluding -byname-implicit is required for Scala 2.13 due to https://github.com/scala/bug/issues/12072
+    "-Xlint:_,-byname-implicit", // without this parameter circe deriveDecoder results in: Block result was adapted via implicit conversion
 )
 // disable generation of scala-<version> folders, we do not need cross compilation
 crossPaths := false

src/main/scala/com/neowit/apexscanner/ast/ApexAstBuilderVisitor.scala
@@ -78,6 +78,7 @@ class ApexAstBuilderVisitor(override val projectOpt: Option[Project],
         getClassLikeNodes.filter(_.isInstanceOf[EnumNode])
             .foreach{
                 case enumNode: EnumNode => EnumNode.addStandardMethods(enumNode)
+                case _ =>
             }
     }
 
@@ -96,10 +97,10 @@ class ApexAstBuilderVisitor(override val projectOpt: Option[Project],
                                 visitChildren(FileNode(project, file, Range(ctx, _documentOffsetPosition)), ctx)
                             case None => NullNode
                         }
-                    case None =>
+                    case _ =>
                         throw new IllegalArgumentException("When parsing whole file - Project must be provided.")
                 }
-            case None =>
+            case _ =>
                 throw new IllegalArgumentException("fileOpt parameter of ApexAstBuilderVisitor must be defined when using visitor with CompilationUnitContext ")
         }
     }

src/main/scala/com/neowit/apexscanner/completion/CompletionFinder.scala
@@ -110,6 +110,7 @@ class CompletionFinder(project: Project) extends LazyLogging {
             case ValueTypeVoid => Seq.empty
             case ValueTypeAnnotation(qualifiedName) => AnnotationNode.getStdAnnotations
             case ValueTypeAny => ???
+            case _ => Seq()
         }
     }
     private def getSymbolsOf(qualifiedName: QualifiedName): Seq[Symbol] = {

src/main/scala/com/neowit/apexscanner/completion/ContextResolver.scala
@@ -169,6 +169,7 @@ class ContextResolver(project: Project, astScopeNode: AstNode, lastAstNode: AstN
                                     // to find definition of node preceding the caret
                                     ???
                             }
+                        case _ => None
                     }
                 } else {
                     ???
@@ -194,7 +195,7 @@ class ContextResolver(project: Project, astScopeNode: AstNode, lastAstNode: AstN
                                 // TODO
                                 ???
                         }
-
+                    case _ => None
                 }
             case _ =>
                 ???

src/main/scala/com/neowit/apexscanner/extlib/impl/stdlib/StdlibLocal.scala
@@ -72,13 +72,13 @@ private class StdlibLocal(project: Project, fileOpt: Option[File] = None) extend
     private def getSourceStream: InputStream = {
         fileOpt match {
             case Some(providedFile) if providedFile.canRead => new FileInputStream(providedFile)
-            case None => getDefaultStream
+            case _ => getDefaultStream
         }
     }
 
     private def getDefaultStream: InputStream = {
         // when reading resource from inside .jar have use getResourceAsStream() instead of getResource()
-        val is = getClass.getClassLoader.getResourceAsStream("apex-api-v50.json")
+        val is = getClass.getClassLoader.getResourceAsStream("apex-api-v51.json")
         if (null != is) {
             is
         } else {

src/main/scala/com/neowit/apexscanner/nodes/ExpressionDotExpressionNode.scala
@@ -112,64 +112,63 @@ case class ExpressionDotExpressionNode(range: Range) extends AbstractExpression 
       * @return
       */
     private def resolveDefinitionFromHead(expressions: Seq[AbstractExpression], actionContext: ActionContext): Seq[AstNode] = {
-        if (expressions.isEmpty) {
-            Seq.empty
-        } else {
-            expressions match {
-                case lst if lst.nonEmpty=>
-                    val head = lst.head
-                    val tail = lst.tail
-                    head match {
-                        case n:IsTypeDefinition => resolveTailDefinitions(n, tail, actionContext)
-                        case n: ThisExpressionNode =>
-                            n.resolveDefinition(actionContext) match {
-                              case Some(_def: IsTypeDefinition) =>
-                                  resolveTailDefinitions(_def, tail, actionContext)
-                              case _ =>
-                                    Seq.empty
-                            }
-                        case n: SuperExpressionNode =>
-                            n.resolveDefinition(actionContext) match {
-                                case Some(_def: IsTypeDefinition) =>
-                                    resolveTailDefinitions(_def, tail, actionContext)
-                                case _ =>
-                                    Seq.empty
-                            }
+        expressions match {
+            case lst if lst.nonEmpty=>
+                val head = lst.head
+                val tail = lst.tail
+                head match {
+                    case n:IsTypeDefinition => resolveTailDefinitions(n, tail, actionContext)
+                    case n: ThisExpressionNode =>
+                        n.resolveDefinition(actionContext) match {
+                            case Some(_def: IsTypeDefinition) =>
+                                resolveTailDefinitions(_def, tail, actionContext)
+                            case _ =>
+                                Seq.empty
+                        }
+                    case n: SuperExpressionNode =>
+                        n.resolveDefinition(actionContext) match {
+                            case Some(_def: IsTypeDefinition) =>
+                                resolveTailDefinitions(_def, tail, actionContext)
+                            case _ =>
+                                Seq.empty
+                        }
 
-                        case n: ArrayIndexExpressionNode =>
-                            n.resolveDefinition(actionContext) match {
-                                case Some(_def: IsTypeDefinition) =>
-                                    resolveTailDefinitions(_def, tail, actionContext)
-                                case _ =>
-                                    Seq.empty
-                            }
-                        case n: LiteralLike =>
-                            n.resolveDefinition(actionContext) match {
-                                case Some(_def: IsTypeDefinition) =>
-                                    resolveTailDefinitions(_def, tail, actionContext)
-                                case _ =>
-                                    Seq.empty
-                            }
-                        case n =>
-                            //head of expression has not been resolved yet, find its definition going UPwards
-                            val finder = new AscendingDefinitionFinder(actionContext)
-                            finder.findDefinition(n, n).headOption match {
-                                case Some(_def: IsTypeDefinition) =>
-                                    resolveTailDefinitions(_def, tail, actionContext)
-                                case _ =>
-                                    n match {
-                                        case identifier @ IdentifierNode(_, _) =>
-                                            identifier.resolveDefinitionByQualifiedName() match {
-                                                case Some(_def: IsTypeDefinition) =>
-                                                    resolveTailDefinitions(_def, tail, actionContext)
-                                                case _ =>
-                                                    Seq.empty
-                                            }
+                    case n: ArrayIndexExpressionNode =>
+                        n.resolveDefinition(actionContext) match {
+                            case Some(_def: IsTypeDefinition) =>
+                                resolveTailDefinitions(_def, tail, actionContext)
+                            case _ =>
+                                Seq.empty
+                        }
+                    case n: LiteralLike =>
+                        n.resolveDefinition(actionContext) match {
+                            case Some(_def: IsTypeDefinition) =>
+                                resolveTailDefinitions(_def, tail, actionContext)
+                            case _ =>
+                                Seq.empty
+                        }
+                    case n =>
+                        //head of expression has not been resolved yet, find its definition going UPwards
+                        val finder = new AscendingDefinitionFinder(actionContext)
+                        finder.findDefinition(n, n).headOption match {
+                            case Some(_def: IsTypeDefinition) =>
+                                resolveTailDefinitions(_def, tail, actionContext)
+                            case _ =>
+                                n match {
+                                    case identifier @ IdentifierNode(_, _) =>
+                                        identifier.resolveDefinitionByQualifiedName() match {
+                                            case Some(_def: IsTypeDefinition) =>
+                                                resolveTailDefinitions(_def, tail, actionContext)
+                                            case _ =>
+                                                Seq.empty
+                                        }
+                                    case _ => Seq.empty
 
-                                    }
-                            }
-                    }
-            }
+                                }
+                        }
+                }
+            case _ => Seq.empty
+
         }
     }
 

src/main/scala/com/neowit/apexscanner/nodes/ExpressionListNode.scala
@@ -32,6 +32,7 @@ case class ExpressionListNode(expressions: Seq[AstNode], range: Range) extends A
         expressions.flatMap{
             case n: AbstractExpression => Option(n)
             case n: FallThroughNode => n.getChildInAst[AbstractExpression](ExpressionNodeType)
+            case _ => Set.empty
         }
     }
 

commit 06169530903d83d799a9dbbfc34c229fb4416447

fix bug introduced in [5934cbc]

src/main/scala/com/neowit/apexscanner/resolvers/AscendingDefinitionFinder.scala
@@ -124,7 +124,7 @@ class AscendingDefinitionFinder(actionContext: ActionContext) extends LazyLoggin
 
         // convert Seq[Future[Option[AstNode]]] to Future[Seq[Option[AstNode]]]
         val allDefNodes = nodeOpts.filter(_.isDefined).map(_.get)
-        allDefNodes.map {
+        allDefNodes.filter {
             case _: AstNode with IsTypeDefinition => true
             case _ => false
         }.map(_.asInstanceOf[AstNode with IsTypeDefinition])